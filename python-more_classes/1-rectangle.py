9d259051518d08507f0b11aa22f248ec7641f657
leyla1223 <leylamuradli2016@gmail.com>
2025-06-28 09:23:35 +0000

    Rectangle init

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is about rectangles with sets and gets"""
+
+
+class Rectangle:
+    """Rectangle properties class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
