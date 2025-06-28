a57e6a2e5c2aea1dfecb774368954bd275b4e9b4
leyla1223 <leylamuradli2016@gmail.com>
2025-06-28 15:29:12 +0000

    qqq

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square module"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
