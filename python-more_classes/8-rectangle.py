abf386e1041156d58487f03b2f98d23a63e58b8a
leyla1223 <leylamuradli2016@gmail.com>
2025-06-28 11:21:04 +0000

    BIG

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -58,8 +58,10 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+
     @staticmethod
-    if not isinstance(rect_1, Rectangle):
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
