a83d4f9e873bf65ea41f0b6b8fa4bc648c0fd3f7
leyla1223 <leylamuradli2016@gmail.com>
2025-07-05 16:22:46 +0000

    ffff

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Task: Write a script that creates a table called first_table in the current database in your MySQL server
+-- This query creates the table only if it does not already exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    nam VARCHAR(256)
+);
