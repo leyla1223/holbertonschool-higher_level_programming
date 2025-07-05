3f4dfbc50d43e4a9b7aebd916a932bd54d8b007d
leyla1223 <leylamuradli2016@gmail.com>
2025-07-05 19:12:22 +0000

    assa

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,8 @@
 -- Task: List all shows without a genre linked
 -- Display title and genre_id (NULL if no genre)
--- Sorted by title asc, genre_id asc
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
