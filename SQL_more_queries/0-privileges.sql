89df00d0ef4518b7a3388cfcaf969ae95ef83ea3
leyla1223 <leylamuradli2016@gmail.com>
2025-07-05 17:49:14 +0000

    tree

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Task: List all privileges of user_0d_1 and user_0d_2 on localhost
+-- This query shows privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- This query shows privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
