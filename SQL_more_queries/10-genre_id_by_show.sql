f9929204bcec70513882906466efe1f713b97ed0
leyla1223 <leylamuradli2016@gmail.com>
2025-07-05 19:02:07 +0000

    hiuwhd

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
 -- Task: List all tv shows that have at least one genre linked
 -- Display: tv_shows.title and tv_show_genres.genre_id
--- Sorted by title asc, genre_id asc
 
+-- Lists all shows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
