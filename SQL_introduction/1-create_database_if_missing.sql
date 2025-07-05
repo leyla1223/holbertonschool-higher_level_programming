74fb4df4232b8bf2866b2e8a85c48aaba7eb0f20
leyla1223 <leylamuradli2016@gmail.com>
2025-07-05 16:03:01 +0000

    ss

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Task: Write a script that creates the database hbtn_0c_0 in your MySQL server
+-- This query creates the database only if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
