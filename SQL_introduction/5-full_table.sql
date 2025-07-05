e533952cd85d8aeb34f480f92eb320651be54f47
leyla1223 <leylamuradli2016@gmail.com>
2025-07-05 16:32:43 +0000

    sss

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Task: Write a script that prints the description of the table first_table from the database hbtn_0c_0
+-- This query shows the full CREATE statement of the table
+SHOW CREATE TABLE first_table;
