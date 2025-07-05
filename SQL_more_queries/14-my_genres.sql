3366fa6ff5b0110c8428390b4e16d391db2bfaf1
leyla1223 <leylamuradli2016@gmail.com>
2025-07-05 19:22:27 +0000

    here

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,9 @@
 -- Task: List all genres of the show Dexter
 -- Display genre names sorted ascending
-SELECT g.name
-FROM genres g
-JOIN tv_show_genres tsg ON g.id = tsg.genre_id
-JOIN tv_shows ts ON ts.id = tsg.tv_show_id
-WHERE ts.title = 'Dexter'
-ORDER BY g.name ASC;
+
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
