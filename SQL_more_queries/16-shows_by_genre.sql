e5cde5d11501752855355283bdcd4e1afed3929e
leyla1223 <leylamuradli2016@gmail.com>
2025-07-05 19:26:24 +0000

    ddjudff

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,13 @@
 -- Task: List all shows with their genres; NULL if no genre
 -- Sorted by show title asc, genre name asc
 
-SELECT tv_shows.title AS title, genres.name AS name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN genres ON tv_show_genres.genre_id = genres.id
-ORDER BY tv_shows.title ASC, genres.name ASC;
+SELECT
+  tv_shows.title,
+  tv_genres.name
+FROM
+  tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY
+  tv_shows.title ASC,
+  tv_genres.name ASC;
