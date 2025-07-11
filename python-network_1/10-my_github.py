f23bd71362ae5931f08fe5a5c83242a8ae5634fd
leyla1223 <leylamuradli2016@gmail.com>
2025-07-11 18:56:11 +0000

    nostop

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script takes GitHub credentials (username and token),
+fetches authenticated user info from GitHub API,
+and prints the user ID if authenticated successfully.
+"""
+
+
+import sys
+import requests
+
+
+def main():
+    """and here we go again"""
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get('id'))
+    else:
+        print("None")
+
+if __name__ == "__main__":
+    main()
