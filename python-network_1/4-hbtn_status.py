236d50090dfd32c90b8367e1d630e47ce99f4d46
leyla1223 <leylamuradli2016@gmail.com>
2025-07-11 18:37:51 +0000

    wewe

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status using requests package
+and displays the body of the response with its type and content.
+"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+    content = response.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
